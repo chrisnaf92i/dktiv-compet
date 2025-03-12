97bc9185dd654f3248ff7098925f5d5cd185045d
chrisnaf92i <nafrere.christopher@gmail.com>
2025-03-12 07:02:42 +0100

    reorganise project structure

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import AuthRouter from './router/auth.routes';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app = express();
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'dist', 'index.html'));
+});
 
 app.use('/api/auth', AuthRouter);
 
